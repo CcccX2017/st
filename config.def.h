9d72c56c61e8168f7139146e6bc46618cb8dc177
WackyChen <nebulapsns@gmail.com>
2022-04-26 21:50:30 +0800

    Modify 'config.def.h' Reset Keybind

--- config.def.h
+++ config.def.h
@@ -216,8 +216,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i =  1} },
+	{ MODKEY,               XK_Page_Down,   kscrolldown,    {.i =  1} },
+	{ MODKEY|ControlMask,   XK_Page_Up,     kscrollup,      {.i =  -1} },
+	{ MODKEY|ControlMask,   XK_Page_Down,   kscrolldown,    {.i =  -1} },
 };
 
 /*
