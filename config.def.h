e56f1a9f6cf0805c3681a3f28bf04359800a9cf5
WackyChen <nebulapsns@gmail.com>
2022-04-26 19:22:52 +0800

    Change Font -> JetBrainsMono Nerd Font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
